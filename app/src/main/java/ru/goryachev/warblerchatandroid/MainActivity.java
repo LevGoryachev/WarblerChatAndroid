fabcff0a996d393e0ac31f09c0060fafe14400af
LevGoryachev <l.goryachev@inbox.ru>
2020-07-23 02:22:59 +0300

    Override onActivityResult()

--- app/src/main/java/ru/goryachev/warblerchatandroid/MainActivity.java
+++ app/src/main/java/ru/goryachev/warblerchatandroid/MainActivity.java
@@ -1,14 +1,45 @@
 package ru.goryachev.warblerchatandroid;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int SIGN_IN_CODE = 1;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == SIGN_IN_CODE){
+            if (resultCode == RESULT_OK){
+                toastLoggedIn ();
+                displayAllMessages();
+            } else {
+                toastNotLoggedIn();
+                finish();
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    private void displayAllMessages() {
+    }
+
+    private void toastLoggedIn () {
+        Toast.makeText(this, "You are logged in", Toast.LENGTH_SHORT).show();
+    }
+
+    private void toastNotLoggedIn () {
+        Toast.makeText(this, "You are not logged in", Toast.LENGTH_SHORT).show();
     }
 }
