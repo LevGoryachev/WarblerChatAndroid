efe7c986ebbebb430bfb5e7536020e418a20c4bc
LevGoryachev <l.goryachev@inbox.ru>
2020-07-24 01:42:16 +0300

    displayAllMessages()

--- app/src/main/java/ru/goryachev/warblerchatandroid/MainActivity.java
+++ app/src/main/java/ru/goryachev/warblerchatandroid/MainActivity.java
@@ -5,11 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Message;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.google.firebase.database.FirebaseDatabase;
+
+import android.text.format.DateFormat;
 
 public class MainActivity extends AppCompatActivity {
 
     private static int SIGN_IN_CODE = 1;
+    private FirebaseListAdapter<Message> adapter;
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -33,6 +43,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void displayAllMessages() {
+        ListView MessagesList = findViewById(R.id.messagesList);
+        adapter = new FirebaseListAdapter<Message>(this, ChatMessage.class, R.layout.list_item, FirebaseDatabase.getInstance().getReference()) {
+            @Override
+            protected void populateView(View view, ChatMessage mModel, int position) {
+                TextView messageUser;
+                TextView messageTime;
+                TextView messageText;
+                messageUser = view.findViewById(R.id.message_user);
+                messageTime = view.findViewById(R.id.message_time);
+                messageText = view.findViewById(R.id.message_text);
+
+                messageUser.setText(mModel.getUserName());
+                messageTime.setText(DateFormat.format("dd-mm-yyyy HH:mm:ss", mModel.getMessageTime()));
+                messageText.setText(mModel.getMessageText());
+
+            }
+        };
     }
 
     private void toastLoggedIn () {
